42cf646a8e822a4247610210eefe643e62ef80c2
BhavikVashi1804 <bhavikvashi1804@gmail.com>
2019-11-09 10:16:58 +0530

    Simple one Box created using Container

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,8 +13,14 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        height: 200.0,
+        width: 170.0,
       ),
     );
   }
