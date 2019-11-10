950b6951acf0e74fd6462df31136b823617c0419
BhavikVashi1804 <bhavikvashi1804@gmail.com>
2019-11-10 08:22:56 +0530

    Milestone 1

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,6 +6,7 @@ import 'reusable_card.dart';
 const double bottomContainerHeight = 80.0;
 const Color bottomContainerColor = Color(0xFFEB1555);
 const Color activeCardColor = Color(0xFF1D1E33);
+const Color inActiveCardColor = Color(0xFF111328);
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,6 +14,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColor = inActiveCardColor;
+  Color femaleCardColor = inActiveCardColor;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,16 +32,26 @@ class _InputPageState extends State<InputPage> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(
-                    bgcolor: activeCardColor,
-                    cardChild: CustomIconContent('MALE', FontAwesomeIcons.mars),
+                  child: GestureDetector(
+                    onTap: () => updateColor(1),
+                    child: ReusableCard(
+                      bgcolor: maleCardColor,
+                      cardChild:
+                          CustomIconContent('MALE', FontAwesomeIcons.mars),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: ReusableCard(
-                    bgcolor: activeCardColor,
-                    cardChild:
-                        CustomIconContent('FEMALE', FontAwesomeIcons.venus),
+                  child: GestureDetector(
+                    onTap: () {
+                      updateColor(2);
+                      print("FEMALE Button is pressed");
+                    },
+                    child: ReusableCard(
+                      bgcolor: femaleCardColor,
+                      cardChild:
+                          CustomIconContent('FEMALE', FontAwesomeIcons.venus),
+                    ),
                   ),
                 ),
               ],
@@ -75,5 +89,24 @@ class _InputPageState extends State<InputPage> {
         ],
       ),
     );
+  }
+
+  void updateColor(int gender) {
+    //1 for Male , 2 for Female
+    setState(() {
+      if (gender == 1) {
+        if (maleCardColor == inActiveCardColor) {
+          maleCardColor = activeCardColor;
+        } else {
+          maleCardColor = inActiveCardColor;
+        }
+      } else if (gender == 2) {
+        if (femaleCardColor == inActiveCardColor) {
+          femaleCardColor = activeCardColor;
+        } else {
+          femaleCardColor = inActiveCardColor;
+        }
+      }
+    });
   }
 }
