a6e3210d98f5c31bdd08a07ec1f3786a426f8672
BhavikVashi1804 <bhavikvashi1804@gmail.com>
2019-11-11 15:02:38 +0530

    Problem fix some file are going to horrible

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -5,18 +5,16 @@ class ReusableCard extends StatelessWidget {
   final Widget cardChild;
   final Function onTapFunction;
 
-  ReusableCard({@required this.bgcolor, this.cardChild, this.onTapFunction});
+  ReusableCard({this.bgcolor, this.cardChild, this.onTapFunction});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        onTapFunction();
-      },
+      onTap: () => onTapFunction,
       child: Container(
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
-          color: bgcolor,
+          color: Color(0xFF1D1E33),
           borderRadius: BorderRadius.circular(10.0),
         ),
         child: cardChild,
